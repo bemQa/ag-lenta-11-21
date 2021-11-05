b7335f4f5eb0251d66ef48a8fc17bf42f961c8a0
Alexander K <niktonezapomnil@gmail.com>
2021-11-05 19:58:25 +0500

    init

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    if($('.eco-slider').length) {
+        $('.eco-slider').slick({
+            swipe: false,
+            dots: false,
+            arrows: true,
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: true,
+            responsive: [
+                {
+                    breakpoint: 1000,
+                    settings: {
+                        swipe: true,
+                        dots: true,
+                        arrows: false
+                    }
+                }
+            ]
+        });
+    }
+
+});
+
+ymaps.ready(mapInit);
+
+function mapInit () {
+    var myMap = new ymaps.Map('map', {
+            center: [56.83, 60.59],
+            zoom: 5,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        objectManager = new ymaps.ObjectManager({
+            clusterize: true,
+            gridSize: 32,
+            clusterDisableClickZoom: true
+        });
+
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+    myMap.geoObjects.add(objectManager);
+    myMap.behaviors.disable('scrollZoom');
+    myMap.controls.add('zoomControl');
+
+    $.ajax({
+        url: "/data.json"
+    }).done(function(data) {
+        objectManager.add(data);
+    });
+
+}
